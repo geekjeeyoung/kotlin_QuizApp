33eeff35e2cf15c13ff392fcaa2fe5f76fe02b2d
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-01-20 18:53:44 +0900

    Update QuizQuestionsActivity.kt

--- app/src/main/java/fun/chezcandy/myquiz/QuizQuestionsActivity.kt
+++ app/src/main/java/fun/chezcandy/myquiz/QuizQuestionsActivity.kt
@@ -1,11 +1,16 @@
 package `fun`.chezcandy.myquiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -21,6 +26,10 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var tvOptionFour: TextView
     private lateinit var btnSubmit: Button
 
+    private lateinit var tvQuestion: TextView
+    private lateinit var ivImage: ImageView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var tvProgress: TextView
 
     // ToDo (Step 3: Create a variable for getting the name from intent.)
 
@@ -43,14 +52,55 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionFour = findViewById(R.id.tv_option_four)
         btnSubmit = findViewById(R.id.btn_submit)
 
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+
         setQuestion()
     }
 
     private fun setQuestion() {
-        TODO("Not yet implemented")
+        val question =
+            mQuestionsList!![mCurrentPosition - 1] // getting the question from the list with the help of current position.
+
+        defaultOptionsView()
+
+        if (mCurrentPosition == mQuestionsList!!.size) {
+            btnSubmit.text = "FINISH"
+        } else {
+            btnSubmit.text = "SUBMIT"
+        }
+
+        progressBar.progress = mCurrentPosition
+        tvProgress.text = "$mCurrentPosition" + "/" + progressBar.getMax()
+
+        tvQuestion.text = question.question
+        ivImage.setImageResource(question.image)
+        tvOptionOne.text = question.optionOne
+        tvOptionTwo.text = question.optionTwo
+        tvOptionThree.text = question.optionThree
+        tvOptionFour.text = question.optionFour
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, tvOptionOne)
+        options.add(1, tvOptionTwo)
+        options.add(2, tvOptionThree)
+        options.add(3, tvOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this@QuizQuestionsActivity,
+                R.drawable.default_option_border_bg
+            )
+        }
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+
     }
 }
